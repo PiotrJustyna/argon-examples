bac00e6681d4441cb6a2126dc9a5b30bfada7127
Piotr Justyna <piotr.justyna@gmail.com>
2023-12-11 17:04:10 +0000

    hello world f# example

---
+++ 2-hello-world/readme.md
@@ -0,0 +1,13 @@
+# 2-hello-world
+
+A hello world f# console application.
+
+![](./img/demo-2023-12-11.png)
+
+## how to
+
+* `./start.sh`
+* `cd hello-world`
+* `dotnet build`
+* `dotnet run`
+* `exit`
---
+++ 2-hello-world/render-documents.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+DOCUMENT_PREVIEW="rendered-documents"
+
+rm -rf "$DOCUMENT_PREVIEW"
+
+mkdir "$DOCUMENT_PREVIEW"
+
+pandoc -F mermaid-filter -o "./$DOCUMENT_PREVIEW/readme.pdf" "./readme.md"
